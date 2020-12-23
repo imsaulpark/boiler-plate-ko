634103e82a78642587c86b79852ee2b32b14696d
seungwoo <sw@bagchan-us-iMac.local>
2020-12-23 23:26:22 +0900

    Lecture 09 Secrue the secret information

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //HEROKU에서의 MONGO_URI와 같은 값
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
-const { User } = require(
-    "./models/User"
-)
+const config = require('./config/key')
+const { User } = require("./models/User")
 
 //body parser가 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 것이므로
 //application/x-www-form-urlencoded 타입으로 된 것을 가져올 수 있게 해주는 함수
@@ -14,13 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jyan:abcd1234@boilerplate.pfoka.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-userNewUrlParser: true, userUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+mongoose.connect(config.mongoURI, {
+useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!`_`')
+  res.send('Hello World!`_`!!')
 })
 
 //회원가입할 때 필요한 정보들을 client에서 받아온 후 db에 넣기
